211e37b1380ddafa604eb09904c4f612e66593bb
Virantha Ekanayake <virantha@Viranthas-MacBook-Air.local>
2013-07-18 14:27:02 -0400

    updated get_dependencies to be cleaner

--- fabfile.py
+++ fabfile.py
@@ -1,16 +1,22 @@
 from fabric.api import *
+import os
  
 env.hosts = ['virantha@96.8.112.217']
   
 def get_dependencies():
     # Get all the local ember.js dependencies
     js_lib_path = "site/js/lib"
+    js_libs = { 'jquery.js': "http://code.jquery.com/jquery-2.0.0.js",
+                'handlebars.js': 'https://raw.github.com/wycats/handlebars.js/1.0.0-rc.4/dist/handlebars.js',
+                'ember.js': 'http://builds.emberjs.com.s3.amazonaws.com/ember-latest.js',
+                'ember-data.js': 'http://builds.emberjs.com.s3.amazonaws.com/ember-data-latest.js',
+                }
     if not os.path.exists(js_lib_path):
         os.makedirs(js_lib_path)
-    local("wget -O site/js/lib/jquery.js http://code.jquery.com/jquery-2.0.0.js")
-    local("wget -O site/js/lib/handlebars.js https://raw.github.com/wycats/handlebars.js/1.0.0-rc.4/dist/handlebars.js")
-    local("wget -O site/js/lib/ember.js http://builds.emberjs.com.s3.amazonaws.com/ember-latest.js")
-    local("wget -O site/js/lib/ember-data.js http://builds.emberjs.com.s3.amazonaws.com/ember-data-latest.js")
+    
+    for tgt, wgetaddr in js_libs.items():
+        # Fetch all the libs
+        local("wget -O %s %s" % (os.path.join(js_lib_path,tgt), wgetaddr))
 
 def copy():
     # make sure the directory is there!
